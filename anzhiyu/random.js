7b37931ffbdc0923cb4b35043893997494cfa63b
mhd250 <mhd250@users.noreply.github.com>
2026-02-27 08:19:00 +0000

    deploy: 7279f0b3843732a1815a458d4a85bcc3bb0f9b40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/27/hello-world/"];function toRandomPost(){
+var posts=["2026/02/27/hello-world/","2026/02/27/未来/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
