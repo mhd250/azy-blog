6637b3e5fb452a03b9a28ba1cb41cff25ba76fec
mhd250 <mhd250@users.noreply.github.com>
2026-06-05 06:36:40 +0000

    deploy: f634412656d60c67772c63c93c2a35c2fae8ca9b

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/28/hello-world/","2026/03/28/未来/"];function toRandomPost(){
+var posts=["2026/06/05/hello-world/","2026/06/05/未来/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
